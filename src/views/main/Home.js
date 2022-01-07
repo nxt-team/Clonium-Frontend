1c4c17175be1d3012ff1f4190e4ae4b332e373f0
doubleM26 <misha.261206@yandex.ru>
2022-01-07 23:36:53 +0300

    очумелое обновление боев на декстопе

--- src/views/main/Home.js
+++ src/views/main/Home.js
@@ -25,7 +25,9 @@ import {
     Icon28RadiowavesAroundOutline,
     Icon28ChevronRightCircleOutline,
     Icon28HistoryForwardOutline,
-    Icon24ChevronCompactRight, Icon28DonateCircleFillYellow,
+    Icon24ChevronCompactRight,
+    Icon28DonateCircleFillYellow,
+    Icon24Switch
 } from '@vkontakte/icons';
 
 import './home.css';
@@ -46,6 +48,8 @@ const numericIndicator = {
     fontSize: 13,
     boxShadow: '0 4px 24px 0 rgb(0 0 0 / 8%), 0 0 12px 0 rgb(0 0 0 / 8%)',
 }
+
+const startupParameters = new URLSearchParams(window.location.search.replace('?', ''))
 
 const Home = ({id, go, isStartTooltip, resetIsStartTooltip, changeActiveModal, goToCreatingRoom, fetchedUser, userBalances, online, updateNeedUsersInFight, updateSecretId, updateNotifications, startupParameters, goIsolated, goToAchievements, goToHistory, goToPage}) => {
 
@@ -305,9 +309,22 @@ const Home = ({id, go, isStartTooltip, resetIsStartTooltip, changeActiveModal, g
                     alignY={"top"}
                 >
                     <div className={'fullContainer'}>
-                        <Title level="1" weight="semibold" style={{marginLeft: 16}} >
-                            Доступные комнаты
-                        </Title>
+                        <div style={{display: "flex", justifyContent: "space-between"}} >
+                            <Title level="1" weight="semibold" style={{marginLeft: 16}} >
+                                Доступные комнаты
+                            </Title>
+
+                            {startupParameters.get('vk_platform') === "desktop_web" &&
+                                <div className={"refreshButton"} onClick={async () => {
+                                    setFights([]);
+                                    const fights = await getFights(fetchedUser)
+                                    setFights(fights)
+                                }}>
+                                    <Icon24Switch width={20} height={20} />
+                                </div>
+                            }
+
+                        </div>
 
                         {renderFights()}
 
